0cf59b8f868ad5d2fdf008a7326281d80e152e33
yuuuo <yuuto_ose@yahoo.co.jp>
2019-02-12 12:49:06 +0900

    fix timer interval

--- AsemSim/Form1.Designer.cs
+++ AsemSim/Form1.Designer.cs
@@ -523,6 +523,7 @@
             // 
             // timer1
             // 
+            this.timer1.Interval = 50;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // statusLabel
