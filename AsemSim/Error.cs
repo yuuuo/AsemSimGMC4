7589b01bac1c1abd195a5acbfd47ab968cefec4d
yuuu <yuuto_ose@yahoo.co.jp>
2019-02-13 18:59:37 +0900

    add Error Message

--- AsemSim/Error.cs
+++ AsemSim/Error.cs
@@ -19,7 +19,32 @@ namespace AsemSim
             ShowMessage("END is not Found");
         }
 
-        private static void ShowMessage(string str)
+		public static void Opcode(int l)
+		{
+			ShowMessage("L:" + l.ToString() + "  Operation is not Found");
+		}
+
+		public static void OprInvalid(int l)
+		{
+			ShowMessage("L:" + l.ToString() + "  Operand is Invalid");
+		}
+
+		public static void OprMany(int l)
+		{
+			ShowMessage("L:" + l.ToString() + "  Too many Operands");
+		}
+
+		public static void LabelMany(int l)
+		{
+			ShowMessage("L:" + l.ToString() + "  Label is Already Used");
+		}
+
+		public static void LabelNotFound(int l)
+		{
+			ShowMessage("L:" + l.ToString() + "  Label is not Found");
+		}
+
+		private static void ShowMessage(string str)
         {
             MessageBox.Show(str, "Assemble Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
